fedd21d2b875d2f8fe7cd23c4aa465364d941684
Dinushika Ranasinghe <101083190+CodesPractice@users.noreply.github.com>
2022-03-08 21:09:38 +0530

    Update 3 lenngthAndCapacity.go

--- slices/3 lenngthAndCapacity.go
+++ slices/3 lenngthAndCapacity.go
@@ -18,5 +18,19 @@ func main() {
 
 	sl2 := []int{2, 3, 5, 7, 2, 9, 1, 0, 3}
 	fmt.Printf("Len %d, Cap %d", len(sl2), cap(sl2)) //Len 9, Cap 9
+	
+	fruitarray := [...]string{"apple", "orange", "grape", "mango", "water melon", "pine apple", "chikoo"}
+	fruitslice := fruitarray[1:3]
+	fmt.Printf("Original Slice \nlength is  %d and capacity is %d\n", len(fruitslice), cap(fruitslice)) //length of is 2 and capacity is 6
+	fruitslice = fruitslice[:cap(fruitslice)]                                                           //re-slicing furitslice till its capacity
+	fmt.Println("After re-slicing \nlength is", len(fruitslice), "and capacity is", cap(fruitslice))
+
+	/*
+		Original Slice
+		length is  2 and capacity is 6
+		After re-slicing
+		length is 6 and capacity is 6
+	*/
+
 
 }
