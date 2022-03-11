941ee0510f2fcd3886195f9afe697ac3f2392446
Dinushika Ranasinghe <101083190+CodesPractice@users.noreply.github.com>
2022-03-11 10:51:14 +0530

    String length

---
+++ Strings/6 stringLength.go
@@ -0,0 +1,34 @@
+//-------------String length------------
+
+// The RuneCountInString(s string) function of the utf8 package
+// used to find the length of the string.
+
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	word1 := "Señor"
+	fmt.Printf("String: %s\n", word1)
+	fmt.Printf("Length: %d\n", utf8.RuneCountInString(word1))
+	fmt.Printf("Number of bytes: %d\n", len(word1))
+
+	fmt.Printf("\n")
+	word2 := "Pets"
+	fmt.Printf("String: %s\n", word2)
+	fmt.Printf("Length: %d\n", utf8.RuneCountInString(word2))
+	fmt.Printf("Number of bytes: %d\n", len(word2))
+}
+
+/*
+String: Señor
+Length: 5
+Number of bytes: 6
+
+String: Pets
+Length: 4
+Number of bytes: 4
+*/
