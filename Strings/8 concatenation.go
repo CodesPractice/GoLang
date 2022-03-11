dec7d08da03a5e65d4288dd940ad14d358bd146e
Dinushika Ranasinghe <101083190+CodesPractice@users.noreply.github.com>
2022-03-11 10:51:50 +0530

    String concatenation

---
+++ Strings/8 concatenation.go
@@ -0,0 +1,18 @@
+//----------------String concatenation-------------
+
+//  + operator used to concatenate strings
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	string1 := "Go"
+	string2 := "is awesome"
+	result := string1 + " " + string2
+	fmt.Println(result)
+}
+
+//Go is awesome
