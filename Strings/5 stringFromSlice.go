ef4bd78016c68918175a0e1ef394a81f5bc24945
Dinushika Ranasinghe <101083190+CodesPractice@users.noreply.github.com>
2022-03-11 10:50:55 +0530

    Creating a string from a slice of runes

---
+++ Strings/5 stringFromSlice.go
@@ -0,0 +1,14 @@
+//-------------Creating a string from a slice of runes------------
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	runeSlice := []rune{0x0053, 0x0065, 0x00f1, 0x006f, 0x0072}
+	str := string(runeSlice)
+	fmt.Println(str)
+}
+
+//Señor
